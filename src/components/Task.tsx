e502aeea5991f89f1e0e4b50e830499ba3c886f8
hansj <june12300039@gmail.com>
2025-07-05 02:19:37 +0900

    fix: 수정/수정완료 계속 수정으로만 뜨게 - 기존에 바뀌던건 디버깅용

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -25,29 +25,13 @@ export default function Task({ task, index, toggleFinished, editTask, removeTask
           <input type="checkbox" checked={task.finished} onChange={() => toggleFinished(task.id)} tabIndex={-1}
             className='w-5 h-5 accent-blue-500' />
         </label>
-        {/* {task.isEditting ? (
-          // 내용 input
-          <input type='text' defaultValue={task.text} id={`task-input-${index}`} ref={task.ref}
-            onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
-              if (event.key === 'Enter') {
-                editTask(task.id);
-              }
-            }}
-            className={`border-2 border-gray-400 focus:border-blue-500 focus:border-2 p-2`} />
-        ) :
-          // 내용 text
-          <span className='task-text'>{task.text}</span>
-          } */}
         <span className='task-text'>{task.text}</span>
       </div>
 
       {/* task 수정/삭제 버튼 */}
       <div className='flex gap-4'>
-        {/* <button onClick={() => editTask(task.id)} className='border-2 border-green-500 hover:bg-green-500 text-white py-1 px-2 rounded'>
-          수정
-        </button> */}
         <button onClick={() => editTask(task.id)} className='border-2 border-green-500 hover:bg-green-500 text-white py-1 px-2 rounded'>
-          {!task.isEditting ? '수정' : '수정 완료'}
+          수정
         </button>
         <button onClick={() => removeTask(task.id)} className='delete'>삭제</button>
       </div>
